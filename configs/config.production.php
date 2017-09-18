32a1708efca74cd24567304e463da2cabedfcc15
Amit Dugar <amit@deforay.com>
2017-09-18 17:21:31 +0530

    Minor update in config.production.php

--- configs/config.production.php
+++ configs/config.production.php
@@ -5,8 +5,10 @@ require_once(__DIR__.'/system.php');
 $HOST = '127.0.0.1';
 $USER = 'root';
 $PASSWORD = 'zaq12345';
+//$PASSWORD = '';
 $DBNAME = 'vl_lab_request';
 $PORT = 3436;
+//$PORT = 3306;
 
 // Uniform Server
 $MYSQLDUMP = __DIR__.'\..\..\core\mysql\bin\mysqldump.exe';
